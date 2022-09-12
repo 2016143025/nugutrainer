007f84a645289b98cb2227b8bc9907da5ab2aae8
Youngjae <sdhkygg@gmail.com>
2022-09-12 21:51:18 +0900

    delete score

--- nuguhome/admin.py
+++ nuguhome/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import gymlocation, trainer,score
+from .models import gymlocation, trainer
 admin.site.register(trainer)
-admin.site.register(score)
 admin.site.register(gymlocation)
