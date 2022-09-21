68ef992228a099ad2d0882d82b084952dd377a44
Youngjae <sdhkygg@gmail.com>
2022-09-21 18:02:17 +0900

    collapse

--- nuguhome/admin.py
+++ nuguhome/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import gymlocation, trainer, score
-admin.site.register(trainer)
+@admin.register(trainer)
+class trainerAdmin(admin.ModelAdmin):
+    list_display= ['id','gym','name','created_at']
+    search_fields = ['name']
+    list_filter = ['created_at']
+#admin.site.register(trainer)
 admin.site.register(score)
 admin.site.register(gymlocation)
